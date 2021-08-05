af0a3305b41c52f117ba6a2872d8e1139913ad6b
Nripen <nchakrawarty@gmail.com>
2021-08-05 23:27:55 +0530

    Create chesters.js

---
+++ chesters.js
@@ -0,0 +1,7 @@
+window.onload = writeMessage;
+function writeMessage() {
+     document.getElementById("stage").innerHTML = "Hello, world!";
+console.log("loaded");
+document.getElementsByClassName("section-22 wf-section").style.display = "none";
+}
+
